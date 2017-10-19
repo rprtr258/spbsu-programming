610a733b3fc197e611bdf8c202f4addbce642c6c
Unknown <poletansky@gmail.com>
2017-10-19 16:35:49 +0300

    5.2 solution

---
+++ 5.2/main.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void printBinary(const int unsigned x) {
+    int unsigned bit = 0x800000;
+    while (bit) {
+        printf(x & bit ? "1" : "0");
+        bit >>= 1;
+    }
+    printf("\n");
+}
+
+int main() {
+    printf("Complement code test\n");
+    
+    printf("Write two numbers from -2\'147\'483\'647 to 2147483648 inclusive\n");
+    int first = 0;
+    int second = 0;
+    scanf("%d %d", &first, &second);
+    
+    int unsigned a = first;
+    int unsigned b = second;
+    
+    printf("Complement code:\n");
+    printBinary(a);
+    printBinary(b);
+    
+    int unsigned sum = a + b;
+    printf("Complement code of sum:\n");
+    printBinary(sum);
+    bool bit = (sum & 0x80000000);
+    int sumNormal = (bit ? (~sum) + 1 : sum);
+    printf("Decimal sum:\n");
+    if (bit)
+        printf("-");
+    printf("%d", sumNormal);
+    return 0;
+}
