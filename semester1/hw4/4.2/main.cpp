c126aa4405ed84d814fc5b5ba7d1d39a98462118
Poletansky Viktor <poletansky@gmail.com>
2017-11-15 23:16:07 +0300

    fixed task

--- semester1/hw4/4.2/main.cpp
+++ semester1/hw4/4.2/main.cpp
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
+int const ALPHABET = 256;
+
 int main() {
-    printf("First letters of each word in \"file.txt\"\n");
+    printf("First occurrences of each letter in each word in \"file.txt\"\n");
     
     FILE *myfile = fopen("file.txt", "r");
     char s[1000];
     
     while (!feof(myfile)) {
         fscanf(myfile, "%s", s);
-        printf("%c ", s[0]);
+        bool were[ALPHABET];
+        for (int i = 0; i < ALPHABET; i++)
+            were[i] = false;
+        for (int i = 0; s[i] != '\0'; i++)
+            if (!were[s[i]]) {
+                printf("%c", s[i]);
+                were[s[i]] = true;
+            }
+        printf(" ");
     }
     
     fclose(myfile);
