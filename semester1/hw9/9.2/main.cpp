3c01db825acecb87244514a3c2528c43fb049acd
Poletansky Viktor <poletansky@gmail.com>
2017-11-29 21:36:16 +0300

    added file check

--- semester1/hw9/9.2/main.cpp
+++ semester1/hw9/9.2/main.cpp
@@ -2,9 +2,21 @@
 #include <string.h>
 #include "filesencode.h"
 
+bool doesFileExist(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+        return false;
+    fclose(file);
+    return true;
+}
+
 int main() {
     printf("Don\'t open \"dontopen.txt\", here is secret info that will be encoded\n");
     printf("Sorry, but this version doesn\'t work correct with one-letter texts\n");
+    if (!doesFileExist("dontopen.txt")) {
+        printf("\"dontopen.txt\" not found\n");
+        return 0;
+    }
     
     encodeFile("dontopen.txt", "encoded.txt", "codeInfo.txt");
     
