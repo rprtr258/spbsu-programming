db3469c91d1a4447a9e7edccf1e655627503ba4b
Poletansky Viktor <poletansky@gmail.com>
2017-11-29 21:28:03 +0300

    added file existance check

--- semester1/hw9/9.1/main.cpp
+++ semester1/hw9/9.1/main.cpp
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include "graph.h"
 
+bool doesFileExist(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+        return false;
+    fclose(file);
+    return true;
+}
+
 int main() {
     printf("War analyzer program\n");
     printf("Make sure you wrote correct info in \"info.txt\"\n\n");
+    if (!doesFileExist("info.txt")) {
+        printf("\"info.txt\" not found\n");
+        return 0;
+    }
     
     Graph *graph = parseFile("info.txt");
     dijkstra(graph);
