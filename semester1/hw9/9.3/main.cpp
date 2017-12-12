43334bd0bffbea8ac22c838bdb50b2fc236f9df9
Poletansky Viktor <poletansky@gmail.com>
2017-12-12 18:16:46 +0300

    best commit ever

--- semester1/hw9/9.3/main.cpp
+++ semester1/hw9/9.3/main.cpp
@@ -2,10 +2,6 @@
 #include <string.h>
 #include "decoder.h"
 
-// TODO: fix one-symbol text
-// TODO: save tree compactly with bits delimiters
-// TODO: turn into utility
-
 int main() {    
     encode("dontopen.txt", "encoded.txt");
     decode("encoded.txt", "decoded.txt");
