d86116cf05841256e96b0dfa9eb9483a23813957
Poletansky Viktor <poletansky@gmail.com>
2017-12-22 14:34:52 +0300

    added test in main()

--- semester1/test3/t3.2.1/main.cpp
+++ semester1/test3/t3.2.1/main.cpp
@@ -1,9 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "priorityQueue.h"
 
 int main() {
+    srand(time(nullptr));
+    printf("Priority queue simple test\n");
+    printf("Elements are (value, priority)\n");
     PriorityQueue *priorityQueue = priorQueueCreate();
-    
+    for (int i = 0; i < 10; i++) {
+        int value = rand() % 100;
+        int priority = (rand() % 100 + 100) % 100;
+        printf("Put (%d, %d) in queue\n", value, priority);
+        enqueue(priorityQueue, value, priority);
+    }
+    for (int i = 0; i < 10; i++) {
+        int value = dequeue(priorityQueue);
+        printf("Got %d from top of queue\n", value);
+    }
     priorQueueDelete(priorityQueue);
     return 0;
 }
