d2f09b524ff28b24b56be4d2ce54a74b30d9633e
Poletansky Viktor <poletansky@gmail.com>
2017-12-24 16:01:59 +0300

    minor regex refactor

--- semester1/hw11/11.1/regex.cpp
+++ semester1/hw11/11.1/regex.cpp
@@ -13,6 +13,57 @@ bool isSignChar(char const symbol) {
     return (symbol == '+' || symbol == '-');
 }
 
+State getNextState(State const curState, char const symbol) {
+    switch (curState) {
+        case initState: {
+            if (isSignChar(symbol))
+                return signState;
+            else
+                return (isdigit(symbol) ? integerPartState : error);
+            break;
+        }
+        case signState: {
+            return (isdigit(symbol) ? integerPartState : error);
+            break;
+        }
+        case integerPartState: {
+            if (symbol == '.')
+                return dotState;
+            else if (symbol == 'E')
+                return exponentSymbolState;
+            else
+                return (isdigit(symbol) ? integerPartState : error);
+            break;
+        }
+        case dotState: {
+            return (isdigit(symbol) ? fractPartState : error);
+            break;
+        }
+        case fractPartState: {
+            if (symbol == 'E')
+                return exponentSymbolState;
+            else
+                return (isdigit(symbol) ? fractPartState : error);
+            break;
+        }
+        case exponentSymbolState: {
+            if (isSignChar(symbol))
+                return exponentSignState;
+            else
+                return (isdigit(symbol) ? exponentState : error);
+            break;
+        }
+        case exponentSignState: {
+            return (isdigit(symbol) ? exponentState : error);
+            break;
+        }
+        case exponentState: {
+            return (isdigit(symbol) ? exponentState : error);
+            break;
+        }
+    }
+}
+
 bool matchesRegex(const char *string) {
     if (string == nullptr)
         return false;
@@ -20,74 +71,7 @@ bool matchesRegex(const char *string) {
     State curState = initState;
     for (int i = 0; string[i] != '\0'; i++) {
         char symbol = string[i];
-        switch (curState) {
-            case initState: {
-                if (isSignChar(symbol))
-                    curState = signState;
-                else if (isdigit(symbol))
-                    curState = integerPartState;
-                else
-                    curState = error;
-                break;
-            }
-            case signState: {
-                if (isdigit(symbol))
-                    curState = integerPartState;
-                else
-                    curState = error;
-                break;
-            }
-            case integerPartState: {
-                if (isdigit(symbol))
-                    curState = integerPartState;
-                else if (symbol == '.')
-                    curState = dotState;
-                else if (symbol == 'E')
-                    curState = exponentSymbolState;
-                else
-                    curState = error;
-                break;
-            }
-            case dotState: {
-                if (isdigit(symbol))
-                    curState = fractPartState;
-                else
-                    curState = error;
-                break;
-            }
-            case fractPartState: {
-                if (isdigit(symbol))
-                    curState = fractPartState;
-                else if (symbol == 'E')
-                    curState = exponentSymbolState;
-                else
-                    curState = error;
-                break;
-            }
-            case exponentSymbolState: {
-                if (isSignChar(symbol))
-                    curState = exponentSignState;
-                else if (isdigit(symbol))
-                    curState = exponentState;
-                else
-                    curState = error;
-                break;
-            }
-            case exponentSignState: {
-                if (isdigit(symbol))
-                    curState = exponentState;
-                else
-                    curState = error;
-                break;
-            }
-            case exponentState: {
-                if (isdigit(symbol))
-                    curState = exponentState;
-                else
-                    curState = error;
-                break;
-            }
-        }
+        curState = getNextState(curState, symbol);
     }
     return isTerminalState(curState);
 }
