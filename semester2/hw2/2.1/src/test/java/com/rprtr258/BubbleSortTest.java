a0a2fdaa00415da4b5367e1415a4dad86b61f2a5
Poletansky Viktor <poletansky@gmail.com>
2018-02-20 23:22:49 +0300

    test added

---
+++ semester2/hw2/2.1/src/test/java/com/rprtr258/BubbleSortTest.java
@@ -0,0 +1,28 @@
+package com.rprtr258;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BubbleSortTest {
+    private boolean isSorted(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[i - 1] > array[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Test
+    public void sortTest() {
+        int[] a = new int[4];
+        a[0] = 3;
+        a[1] = 1;
+        a[2] = 4;
+        a[3] = 2;
+        SortStrategy ss = new BubbleSort();
+        ss.sort(a);
+        assertTrue(isSorted(a));
+    }
+}
