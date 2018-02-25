c620896406e24caacbf1f7569aafc233bd1f7e6a
Poletansky Viktor <poletansky@gmail.com>
2018-02-25 11:05:29 +0300

    lazy calculation of expression

--- semester2/hw2/2.3/src/main/java/com/rprtr258/Expression.java
+++ semester2/hw2/2.3/src/main/java/com/rprtr258/Expression.java
@@ -4,32 +4,37 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Expression {
-    private ArrayList<String> tokens;
+    private ArrayList<String> tokens = new ArrayList<>();
+    private boolean evaluated = false;
+    private int result = 0;
 
     public Expression(String expr) {
         String exprCopy = expr;
         for (String op : new String[]{"+", "-", "*", "/"})
             exprCopy = exprCopy.replace(op, " " + op + " ");
-        tokens = new ArrayList<>();
         StringTokenizer stringTokenizer = new StringTokenizer(exprCopy, " ", false);
         while (stringTokenizer.hasMoreTokens())
             tokens.add(stringTokenizer.nextToken());
     }
 
     public int eval() {
-        Stack<String> stack = new StackOnLinkedList<>();
-        for (int i = tokens.size() - 1; i >= 0; i--) {
-            String token = tokens.get(i);
-            if (isOperator(token)) {
-                int arg1 = Integer.parseInt(stack.pop());
-                int arg2 = Integer.parseInt(stack.pop());
-                int res = applyOperator(token, arg1, arg2);
-                stack.push(Integer.toString(res));
-            } else {
-                stack.push(token);
+        if (!evaluated) {
+            Stack<String> stack = new StackOnLinkedList<>();
+            for (int i = tokens.size() - 1; i >= 0; i--) {
+                String token = tokens.get(i);
+                if (isOperator(token)) {
+                    int arg1 = Integer.parseInt(stack.pop());
+                    int arg2 = Integer.parseInt(stack.pop());
+                    int res = applyOperator(token, arg1, arg2);
+                    stack.push(Integer.toString(res));
+                } else {
+                    stack.push(token);
+                }
             }
+            result = Integer.parseInt(stack.top());
+            evaluated = true;
         }
-        return Integer.parseInt(stack.top());
+        return result;
     }
 
     private boolean isOperator(String token) {
