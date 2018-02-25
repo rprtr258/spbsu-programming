70042e47fb2035353dc96514059613e2ac846d61
Poletansky Viktor <poletansky@gmail.com>
2018-02-25 11:16:59 +0300

    incorrect operand handling added

--- semester2/hw2/2.3/src/main/java/com/rprtr258/Main.java
+++ semester2/hw2/2.3/src/main/java/com/rprtr258/Main.java
@@ -7,9 +7,21 @@ public class Main {
     
     public static void main(String[] args) {
         System.out.print("Write expression in prefix form to calc, unary minus is not supported(e.g. +2 2):\n");
-        String str = in.nextLine();
-        Expression expr = new Expression(str);
+        Expression expr = inputExpression();
         int result = expr.eval();
         System.out.println("Result is: " + result);
+    }
+
+    private static Expression inputExpression() {
+        while (true) {
+            String str = in.nextLine();
+            Expression expr = new Expression(str);
+            try {
+                expr.eval();
+                return expr;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Incorrect operator found. Please retype:\n");
+            }
+        }
     }
 }
