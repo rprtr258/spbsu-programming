b2bddb43de49366fbb88955bd1e80c54fb84c763
rprtr258 <poletansky@gmail.com>
2018-03-06 11:44:59 +0300

    Update NumberNode.java

--- semester2/hw3/3.2/src/main/java/com/rprtr258/NumberNode.java
+++ semester2/hw3/3.2/src/main/java/com/rprtr258/NumberNode.java
@@ -1,18 +1,17 @@
 package com.rprtr258;
 
 public class NumberNode extends TreeNode {
-    public NumberNode(String data) {
-        super(data);
-    }
-
+    @Override
     public int calc() {
         return Integer.parseInt(data);
     }
 
+    @Override
     public String infixForm(boolean printBrackets) {
         return data;
     }
-
+    
+    @Override
     public String toString() {
         return data;
     }
