d0b7616828fd67092e2f68e5449e2f87fe01c96f
Poletansky Viktor <poletansky@gmail.com>
2018-04-03 18:19:13 +0300

    init

---
+++ semester2/hw6/6.1/src/main/java/com/rprtr258/FirstPartTasks.java
@@ -0,0 +1,109 @@
+package com.rprtr258;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public final class FirstPartTasks {
+    // Список названий альбомов
+    public static List<String> allNames(Stream<Album> albums) {
+        return albums.map(Album::getName)
+                     .collect(Collectors.toList());
+    }
+
+    // Список названий альбомов, отсортированный лексикографически по названию
+    public static List<String> allNamesSorted(Stream<Album> albums) {
+        return albums.map(Album::getName)
+                     .sorted()
+                     .collect(Collectors.toList());
+    }
+
+    // Список треков, отсортированный лексикографически по названию, включающий все треки альбомов из 'albums'
+    public static List<String> allTracksSorted(Stream<Album> albums) {
+        return albums.flatMap(album -> album.getTracks().stream())
+                     .map(Track::getName)
+                     .sorted()
+                     .collect(Collectors.toList());
+    }
+
+    // Список альбомов, в которых есть хотя бы один трек с рейтингом более 95, отсортированный по названию
+    public static List<Album> sortedFavorites(Stream<Album> albums) {
+        return albums.filter(album -> album.getTracks().stream().anyMatch(track -> track.getRating() > 95))
+                     .sorted(Comparator.comparing(Album::getName))
+                     .collect(Collectors.toList());
+    }
+
+    // Сгруппировать альбомы по артистам
+    public static Map<Artist, List<Album>> groupByArtist(Stream<Album> albums) {
+        return albums.collect(Collectors.groupingBy(Album::getArtist, Collectors.toList()));
+    }
+
+    // Сгруппировать альбомы по артистам (в качестве значения вместо объекта 'Artist' использовать его имя)
+    public static Map<Artist, List<String>> groupByArtistMapName(Stream<Album> albums) {
+        return albums.collect(Collectors.groupingBy(Album::getArtist, Collectors.mapping(Album::getName, Collectors.toList())));
+    }
+
+    // Число повторяющихся альбомов в потоке
+    public static long countAlbumDuplicates(Stream<Album> albums) {
+        List<String> list = albums.map(Album::getName).collect(Collectors.toList());
+        Map<String, Integer> count = new TreeMap<>();
+        for (String albumName : list) {
+            count.put(albumName, 0);
+        }
+        for (String albumName : list) {
+            Integer quantity = count.get(albumName);
+            count.replace(albumName, quantity + 1);
+        }
+        return list.stream().filter(albumName -> count.get(albumName) > 1)
+                            .distinct()
+                            .count();
+    }
+
+    // Альбом, в котором максимум рейтинга минимален
+    // (если в альбоме нет ни одного трека, считать, что максимум рейтинга в нем --- 0)
+    public static Optional<Album> minMaxRating(Stream<Album> albums) {
+        return albums.min(Comparator.comparing(FirstPartTasks::getMaxRating));
+    }
+
+    // Список альбомов, отсортированный по убыванию среднего рейтинга его треков (0, если треков нет)
+    public static List<Album> sortByAverageRating(Stream<Album> albums) {
+        return albums.sorted(Comparator.comparingDouble(FirstPartTasks::getAverageRating))
+                     .collect(Collectors.toList());
+    }
+
+    // Произведение всех чисел потока по модулю 'modulo'
+    // (все числа от 0 до 10000)
+    public static int moduloProduction(IntStream stream, int modulo) {
+        return stream.reduce((x, y) -> (x * y) % modulo)
+                     .orElse(1);
+    }
+
+    // Вернуть строку, состояющую из конкатенаций переданного массива, и окруженную строками "<", ">"
+    // см. тесты
+    public static String joinTo(String... strings) {
+        StringJoiner j = new StringJoiner(", ", "<", ">");
+        Arrays.asList(strings).forEach(j::add);
+        return j.toString();
+    }
+
+    // Вернуть поток из объектов класса 'clazz'
+    public static <R> Stream<R> filterIsInstance(Stream<?> stream, Class<R> clazz) {
+        return stream.filter(clazz::isInstance).map(clazz::cast);
+    }
+
+    private FirstPartTasks() {}
+
+    private static Integer getMaxRating(Album album) {
+        return album.getTracks().stream().map(Track::getRating)
+                .max(Comparator.comparingInt(x -> x))
+                .orElse(0);
+    }
+
+    private static Double getAverageRating(Album album) {
+        double averageRating = album.getTracks().stream().map(Track::getRating)
+                .collect(Collectors.averagingInt(x -> x));
+        return (-1) * averageRating;
+    }
+}
---
+++ semester2/hw6/6.1/src/test/java/com/rprtr258/SecondPartTasksTest.java
@@ -0,0 +1,28 @@
+package com.rprtr258;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static com.rprtr258.SecondPartTasks.*;
+
+public class SecondPartTasksTest {
+    @Test
+    public void testFindQuotes() {
+        fail();
+    }
+
+    @Test
+    public void testPiDividedBy4() {
+        fail();
+    }
+
+    @Test
+    public void testFindPrinter() {
+        fail();
+    }
+
+    @Test
+    public void testCalculateGlobalOrder() {
+        fail();
+    }
+}
