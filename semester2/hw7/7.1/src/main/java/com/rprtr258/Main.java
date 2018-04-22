d706f073ce11c9f0dc80801f2bebaafaae48ea97
Poletansky Viktor <poletansky@gmail.com>
2018-04-22 19:49:51 +0300

    added test and docs

--- semester2/hw7/7.1/src/main/java/com/rprtr258/Main.java
+++ semester2/hw7/7.1/src/main/java/com/rprtr258/Main.java
@@ -9,13 +9,20 @@ import java.util.Arrays;
 
 import static java.util.stream.Collectors.joining;
 
+/**
+ * Main application class.
+ */
 public class Main {
+    /**
+     * Entry point.
+     * @param args command line arguments.
+     */
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage:");
             System.out.println("gradle run -Pargs=\"CLASS_FILE\"");
             System.out.println("Example:");
-            System.out.println("gradle run -Pargs=\"test_files/com.rprtr258.AVLTree\"");
+            System.out.println("gradle run -Pargs=\"build/classes/main/java/com.rprtr258.Main\"");
             return;
         }
         try {
@@ -27,6 +34,30 @@ public class Main {
         }
     }
 
+    /**
+     * Returns code of given class.
+     * @param clazz <b>Class</b> object.
+     * @return class pseudo code.
+     */
+    public static String getClassCode(Class clazz) {
+        String result = "";
+        result += printClassDeclaration(clazz);
+        result += "    //Fields\n";
+        result += printClassFields(clazz);
+        result += "    //Constructors\n";
+        result += printClassConstructors(clazz);
+        result += "    //Methods\n";
+        result += printClassMethods(clazz);
+        result += "}";
+        return result;
+    }
+
+    /**
+     * Tries to print class.
+     * @param path path to .class file.
+     * @throws MalformedURLException if couldn't convert it to URL.
+     * @throws ClassNotFoundException if class was not found.
+     */
     private static void tryPrintClass(String path) throws MalformedURLException, ClassNotFoundException {
         int j = path.lastIndexOf('/');
         String className = path.substring(j + 1);
@@ -39,36 +70,43 @@ public class Main {
         System.out.printf("Class name: %s\n", className);
         System.out.printf("Class directory: %s\n", classDirFile.getAbsolutePath());
         System.out.print("\n");
-        printClass(loadedClass);
+        String classCode = getClassCode(loadedClass);
+        System.out.println(classCode);
     }
 
-    private static void printClass(Class clazz) {
-        printClassDeclaration(clazz);
-        System.out.println("    //Fields");
-        printClassFields(clazz);
-        System.out.println("    //Constructors");
-        printClassConstructors(clazz);
-        System.out.println("    //Methods");
-        printClassMethods(clazz);
-        System.out.println("}");
-    }
-
-    private static void printClassDeclaration(Class clazz) {
+    /**
+     * Prints class's declaration.
+     * @param clazz <b>Class</b> object.
+     * @return class's declaration code.
+     */
+    private static String printClassDeclaration(Class clazz) {
         String className = clazz.getSimpleName();
         String parentClassName = clazz.getSuperclass().getSimpleName();
         String implementedList = Arrays.stream(clazz.getInterfaces()).map(Class::getSimpleName).collect(joining(", "));
         if (!"".equals(implementedList))
             implementedList = " implements " + implementedList;
-        System.out.printf("class %s extends %s%s {\n", className, parentClassName, implementedList);
+        return String.format("class %s extends %s%s {\n", className, parentClassName, implementedList);
     }
 
-    private static void printClassFields(Class clazz) {
-        for (Field field : clazz.getDeclaredFields()) {
-            System.out.printf("    %s %s %s;\n", getPrivacyModifier(field.getModifiers()), field.getType().getSimpleName(), field.getName());
-        }
+    /**
+     * Prints class's fields.
+     * @param clazz <b>Class</b> object.
+     * @return class's fields.
+     */
+    private static String printClassFields(Class clazz) {
+        StringBuilder result = new StringBuilder();
+        for (Field field : clazz.getDeclaredFields())
+            result.append(String.format("    %s %s %s;\n", getPrivacyModifier(field.getModifiers()), field.getType().getSimpleName(), field.getName()));
+        return result.toString();
     }
 
-    private static void printClassConstructors(Class clazz) {
+    /**
+     * Prints class's constructors.
+     * @param clazz <b>Class</b> object.
+     * @return class's constructors.
+     */
+    private static String printClassConstructors(Class clazz) {
+        StringBuilder result = new StringBuilder();
         String className = clazz.getSimpleName();
         for (Constructor constructor : clazz.getDeclaredConstructors()) {
             String privacyModifier = getPrivacyModifier(constructor.getModifiers());
@@ -78,11 +116,18 @@ public class Main {
             if (!"".equals(staticModifier))
                 staticModifier += " ";
             String argsList = getArgsList(constructor);
-            System.out.printf("    %s%s%s(%s);\n", privacyModifier, staticModifier, className, argsList);
+            result.append(String.format("    %s%s%s(%s);\n", privacyModifier, staticModifier, className, argsList));
         }
+        return result.toString();
     }
 
-    private static void printClassMethods(Class clazz) {
+    /**
+     * Prints class's methods.
+     * @param clazz <b>Class</b> object.
+     * @return class's methods.
+     */
+    private static String printClassMethods(Class clazz) {
+        StringBuilder result = new StringBuilder();
         for (Method method : clazz.getDeclaredMethods()) {
             String returnTypeName = method.getReturnType().getSimpleName();
             String methodName = method.getName();
@@ -96,14 +141,25 @@ public class Main {
             String throwList = Arrays.stream(method.getExceptionTypes()).map(Class::getSimpleName).collect(joining(", "));
             if (!"".equals(throwList))
                 throwList = " throws " + throwList;
-            System.out.printf("    %s%s%s %s(%s)%s;\n", privacyModifier, staticModifier, returnTypeName, methodName, argsList, throwList);
+            result.append(String.format("    %s%s%s %s(%s)%s;\n", privacyModifier, staticModifier, returnTypeName, methodName, argsList, throwList));
         }
+        return result.toString();
     }
 
-    private static String getArgsList(Executable constructor) {
-        return Arrays.stream(constructor.getParameterTypes()).map(Class::getSimpleName).collect(joining(", "));
+    /**
+     * Returns argumentss list of given method/constructor.
+     * @param executor executable thing.
+     * @return list of arguments joined with comma.
+     */
+    private static String getArgsList(Executable executor) {
+        return Arrays.stream(executor.getParameterTypes()).map(Class::getSimpleName).collect(joining(", "));
     }
 
+    /**
+     * Returns privacy modifier as string encoded in <i>mod</i>.
+     * @param mod modifiers bitmask.
+     * @return privacy modifier.
+     */
     private static String getPrivacyModifier(int mod) {
         if (Modifier.isPublic(mod))
             return "public";
@@ -114,6 +170,11 @@ public class Main {
         return "";
     }
 
+    /**
+     * Returns static modifier as string encoded in <i>mod</i>.
+     * @param mod modifiers bitmask.
+     * @return static modifier as string.
+     */
     private static String getStaticModifier(int mod) {
         if (Modifier.isStatic(mod))
             return "static";
