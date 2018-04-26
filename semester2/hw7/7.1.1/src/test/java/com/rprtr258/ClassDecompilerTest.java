fb60ac6ac656591338270d72e9f34c327a1e2786
Poletansky Viktor <poletansky@gmail.com>
2018-04-26 22:42:23 +0300

    tests fixed

--- semester2/hw7/7.1.1/src/test/java/com/rprtr258/ClassDecompilerTest.java
+++ semester2/hw7/7.1.1/src/test/java/com/rprtr258/ClassDecompilerTest.java
@@ -4,29 +4,25 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
 
 public class ClassDecompilerTest {
     @Test
     public void getClassCodeTest() {
         String[] mainCode = ClassDecompiler.getClassCode(ClassDecompiler.class).split("\n");
         String[] expected = {
-                "class ClassDecompiler extends Object {",
-                "    //Fields",
-                "    //Constructors",
-                "    public ClassDecompiler();",
-                "    //Methods",
-                "    public static void main(String[]);",
-                "    private static String getStaticModifier(int);",
-                "    private static String printClassMethods(Class);",
-                "    private static String printClassConstructors(Class);",
-                "    private static String getPrivacyModifier(int);",
-                "    private static String printClassDeclaration(Class);",
-                "    private static String printClassFields(Class);",
-                "    private static String getArgsList(Executable);",
-                "    public static String getClassCode(Class);",
-                "    private static void tryPrintClass(String) throws MalformedURLException, ClassNotFoundException;",
-                "}"
+            "class ClassDecompiler extends Object {",
+            "    //Constructors",
+            "    public ClassDecompiler();",
+            "    //Methods",
+            "    private static String getClassMethods(Class<T>);",
+            "    private static String getClassConstructors(Class<T>);",
+            "    private static String getClassDeclaration(Class<T>);",
+            "    private static String getClassFields(Class<T>);",
+            "    private static String getArgsList(Executable);",
+            "    public static String getClassCode(Class<T>);",
+            "    private static String getGenericClassName(Class<T>);",
+            "}"
         };
         Arrays.sort(mainCode);
         Arrays.sort(expected);
