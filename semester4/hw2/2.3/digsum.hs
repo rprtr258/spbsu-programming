790a2497a852f4519700cbc7ae1638a0117d66b8
Viktor Poletansky <poletansky@gmail.com>
2019-02-25 01:15:44 +0300

    Update digsum.hs

--- semester4/hw2/2.3/digsum.hs
+++ semester4/hw2/2.3/digsum.hs
@@ -4,3 +4,8 @@ import Data.Char
 
 digsum :: Integer -> Integer
 digsum n = foldl (+) 0 $ map (\x -> toInteger $ ord x - ord '0') $ show n
+
+digsum' :: Integer -> Integer
+digsum' n 
+    | n < 10    = n
+    | otherwise = n `mod` 10 + digsum' (n `div` 10)
