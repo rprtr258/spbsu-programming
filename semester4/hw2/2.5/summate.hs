c8e7e998dafe5cc3acd3be4ec2e85e5b55c5e6a1
Viktor Poletansky <poletansky@gmail.com>
2019-02-25 09:43:07 +0300

    Update summate.hs

--- semester4/hw2/2.5/summate.hs
+++ semester4/hw2/2.5/summate.hs
@@ -1,18 +1,23 @@
 module Summate where
 
+threeSummate :: [Integer] -> [Integer] -> [Integer] -> [Integer]
+threeSummate xs ys zs = summate [xs, ys, zs]
+
+threeSummate' :: [Integer] -> [Integer] -> [Integer] -> [Integer]
+threeSummate' xs ys zs = summate' [xs, ys, zs]
+
 summate :: [[Integer]] -> [Integer]
 summate lst = foldl (zipWith (+)) (addPadding []) $ map addPadding lst
     where
     maxLen = foldl max 0 $ map length lst
     addPadding = format maxLen
-
-format :: Int -> [Integer] -> [Integer]
-format n xs = xs ++ (take (n - (length xs)) $ repeat 0)
+    format :: Int -> [Integer] -> [Integer]
+    format n xs = xs ++ (take (n - (length xs)) $ repeat 0)
 
 summate' :: [[Integer]] -> [Integer]
 summate' lst = foldl add [] lst
-
-add :: [Integer] -> [Integer] -> [Integer]
-add [] lst = lst
-add lst [] = lst
-add (x:xs) (y:ys) = x + y : add xs ys
+    where
+    add :: [Integer] -> [Integer] -> [Integer]
+    add [] lst = lst
+    add lst [] = lst
+    add (x:xs) (y:ys) = x + y : add xs ys
