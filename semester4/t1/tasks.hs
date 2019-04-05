5809e6f1506cef574d19544347c03556c02eb918
Viktor Poletansky <poletansky@gmail.com>
2019-04-05 18:24:27 +0300

    Update tasks.hs

--- semester4/t1/tasks.hs
+++ semester4/t1/tasks.hs
@@ -2,6 +2,7 @@ import Data.List
 --t1.1
 lst :: [Integer]
 lst = 1:(-1):lst
+
 lst' :: [Integer]
 lst' = zipWith (*) [1..] lst
 
@@ -11,22 +12,24 @@ angle_matrix n = [row k | k <- [1..n]] where
   row k = [max i k | i <- [1..n]]
 
 --t1.3
-sizes :: [Int]
-sizes = -1:map (2+) sizes
 draw_diamond :: Int -> [[Char]]
 draw_diamond n = [row k | k <- [1..n]] ++ [row (n - k) | k <- [1..n-1]] where
   row :: Int -> [Char]
-  row k = take (n - k) (repeat ' ') ++ (take (sizes!!k) (repeat 'x'))
+  row k = (take (n - k) $ repeat ' ') ++ (take (2 * k - 1) $ repeat 'x')
+
+print_diamond :: Int -> IO()
+print_diamond n = putStrLn $ intercalate "\n" $ draw_diamond n
+
 main :: IO ()
 main = do
   putStrLn "Enter side size: "
   input <- getLine
   let size = (read input :: Int)
-  putStrLn $ intercalate "\n" $ draw_diamond size
+  print_diamond size
 
 --t1.4
 supermap :: [a] -> (a -> [b]) -> [b]
-supermap lst f = concat (map f lst)
+supermap lst f = concat $ map f $ lst
 
 --t1.5
 check_all :: (a -> Bool) -> [a] -> Bool
