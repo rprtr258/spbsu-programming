eda9f312bfabe5123cfc60bb9826d787215ee032
Viktor Poletansky <poletansky@gmail.com>
2019-04-05 10:03:04 +0300

    Add type declaration

--- semester4/hw3/3.5/fib_acc.hs
+++ semester4/hw3/3.5/fib_acc.hs
@@ -1,4 +1,6 @@
+fib :: Integer -> Integer
 fib = fib_helper (0, 1) where
+  fib_helper :: (Ord a, Num a, Num b) => (b, b) -> a -> b
   fib_helper (a, b) n
     | n == 0 = a
     | n > 0  = fib_helper (b, a + b) (n - 1)
